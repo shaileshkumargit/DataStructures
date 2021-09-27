514099f5927d44dce26a8ba6a4e1a5e70e8a2275
Shailesh Kumar B M <91237210+shaileshkumargit@users.noreply.github.com>
2021-09-27 14:59:43 +0530

    added Array1.cs : contains asecending and descending order program.

--- DataStructures/Array1.cs
+++ DataStructures/Array1.cs
@@ -5,12 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructures
-{
+{ //sort an array elements in an ascending and descending order without using built-in functions.
     class Array1
     {
-        static void Main(string[] args)
+        public void SortArray(int[] arr)
         {
+            int swap = 0;
+            //To read elements of an array.
+            for (int i = 0; i < arr.Length; i++)
+            {//to perform comparison of index values and swap accordingly.
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] >= arr[j])
+                    {
+                        swap = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = swap;
+                    }
+                }
+                Console.Write(arr[i] + " ");
+            }
+        }
+        public static void DescindingOrder(int[] arr)
+        {
+            int swap = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[i] <= arr[j])
+                    {
+                        swap = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = swap;
+                    }
+                }
+                Console.Write(" "  + arr[i]);
+            }
+        }
 
+            static void Main(string[] args)
+        {
+            int[] num = { 22, 50, 11, 2, 49, 79, 90 };
+            Array1 p = new Array1();
+            Console.WriteLine("Elements are sorted in Ascending order: ");
+            p.SortArray(num);
+            Console.WriteLine(" \n");
+            Console.WriteLine("Elements are sorted in Descending order: ");
+            DescindingOrder(num);
+            Console.ReadLine();
         }
     }
 }
